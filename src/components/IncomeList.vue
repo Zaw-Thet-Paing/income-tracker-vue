0980c1e983166a183d81703cacf5797984380d6c
Zaw-Thet-Paing <zawthetpaing333@gmail.com>
2024-02-26 14:20:41 +0630

    Income Tracker

---
+++ src/components/IncomeList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="income-list">
+    <IncomeItem v-for="data in state.sortedIncome" :key="data.id" :income="data" @remove-item="removeItem" />
+  </div>
+</template>
+
+<script>
+import IncomeItem from './IncomeItem.vue'
+export default {
+    name: 'IncomeList', 
+    components: {IncomeItem},
+    props: {
+        state: Object
+    },
+    setup(props, {emit}){
+      function removeItem(id){
+        emit('remove-item', id)
+      }
+
+      return {
+        removeItem
+      }
+    }
+}
+</script>
+
+<style scoped>
+    .income-list {
+        margin-top: 30px;
+        padding: 15px;
+    }
+</style>
